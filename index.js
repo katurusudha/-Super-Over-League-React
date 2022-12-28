2e28005411894775a5af9e95017ed1f6614ac0c0
katurusudha <sudhakaturu570@gmail.com>
2022-12-28 09:05:31 +0000

    done

---
+++ index.js
@@ -0,0 +1,12 @@
+const element = (
+//   Write your code here.
+<h1 className="heading">Super Over League</h1>
+
+<div className="containers"> 
+    <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" className="image"/>
+<img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" className="image"></img>
+</div>
+
+)
+
+ReactDOM.render(element, document.getElementById('root'))
